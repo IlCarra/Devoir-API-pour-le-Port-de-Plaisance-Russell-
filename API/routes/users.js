63a19a1fb9ca03465ebd134d8b2ae49e0e279680
IlCarra <stevan.carrara@gmail.com>
2025-04-14 13:19:36 +0200

    Ajout de l'authentification et la route

--- API/routes/users.js
+++ API/routes/users.js
@@ -7,4 +7,5 @@ router.put('/add', service.add);
 router.patch('/:id', service.update);
 router.delete('/:id', service.delete); 
 
+router.post('/authenticate', service.authenticate);
 module.exports = router;
